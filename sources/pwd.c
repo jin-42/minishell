787d07fbaf5eb009760b8f3b9902a354e4db8238
Sarah Ponthus <sponthus@z4r6p5.42lyon.fr>
2024-03-21 16:50:14 +0100

    mini changement pwd

--- sources/pwd.c
+++ sources/pwd.c
@@ -6,18 +6,28 @@
 /*   By: sponthus <sponthus@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/18 13:34:32 by sponthus          #+#    #+#             */
-/*   Updated: 2024/03/21 16:47:05 by sponthus         ###   ########lyon.fr   */
+/*   Updated: 2024/03/21 16:49:54 by sponthus         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-int	pwd(t_data *data, char **args) // Fully working
+int	pwd(t_data *data, char **args) // A check depuis les ajouts des args
 {
 	char	*val;
 	int		i;
 
+	i = 0;
 	(void)data;
+	while (args && args[i])
+	{
+		if (i == 1)
+		{
+			if (ft_strchr(args[i], '-') != NULL)
+				return (printf("pwd: %s: invalid option\n", args[i]), 1);
+		}
+		i++;
+	}
 	val = getcwd(NULL, 0);
 	if (val == NULL)
 	{
@@ -26,14 +36,6 @@ int	pwd(t_data *data, char **args) // Fully working
 	}
 	printf("%s\n", val);
 	free(val);
-	while (args && args[i])
-	{
-		if (i == 1)
-		{
-			if (ft_strchr(args[i], '-') != NULL)
-				printf("pwd: %s: invalid option\n", args[i]);
-		}
-		i++;
-	}
 	return (0);
 }
+
