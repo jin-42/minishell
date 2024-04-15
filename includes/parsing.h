ba708e8c9e2cc2a791a2b94bea4501170ad5a284
Jin <109819822+jin-42@users.noreply.github.com>
2024-04-15 08:09:40 +0000

    Update parsing.h

--- includes/parsing.h
+++ includes/parsing.h
@@ -1,56 +1,38 @@
 #ifndef PARSING_H
 # define PARSING_H
 
-typedef enum
-  {
-    TOKEN_EAT,
-    TOKEN_DQ,
-    TOKEN_SQ,
-    TOKEN_SEMI,
-    TOKEN_DBL_SEMI,
-    TOKEN_OPERATOR,
-    TOKEN_NEG,
-    TOKEN_DBLAND,
-    TOKEN_DBLOR,
-    TOKEN_PIPE,
-    TOKEN_OP_ARITHM,
-    TOKEN_AND,
-    TOKEN_OP_BRA,
-    TOKEN_CL_BRA,
-    TOKEN_OP_PAR,
-    TOKEN_CL_PAR,
-    TOKEN_REDIR,
-    TOKEN_ASS_WORD,
-    TOKEN_OP_CMD,
-    TOKEN_OP_CURLY,
-    TOKEN_CL_CURLY,
-    TOKEN_EQUAL,
-    TOKEN_STRING,
-    TOKEN_NAME,
-    TOKEN_FOR,
-    TOKEN_WHILE,
-    TOKEN_NULL
-  } e_token_type;
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <string.h>
 
 
-typedef struct s_oplist
+typedef enum e_bash_op
 {
-  const char* op;
-  uint32_t size;
-  e_token_type type;
-} t_oplist;
+	VAR = 0,
+	VAR_IN_QUOTE,
+	OP,
+	STRING
+} e_token_type;
 
-typedef struct s_lexer_token
+typedef struct s_token
 {
-  char *content; 
-  uint32_t size;
-  e_token_type type;
-} t_lexer_token;
+	char *str;
+	e_token_type type;
+	struct s_token *next;
+} t_list;
 
-typedef struct s_lexer
+typedef struct s_inter
 {
-  t_lexer_token *tokens;
-  uint32_t size;
-} t_lexer;
+	char			**arr;
+	char			*infile;
+	char			*outfile;
+	char			*heredoc;
+	char			*append;
+	struct s_inter	*next;
+}					t_inter;
 
+
+t_inter	*_parse(t_list *token);
+void _print_inter(const t_inter *node);
 #endif 
